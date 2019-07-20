f964f4ff54f9a6991501a01812d41122dfffcd1f
Sergio Rodriguez <pekeno1811@gmail.com>
2019-07-20 12:59:07 +0200

    PushPrueba

---
+++ Assets/ScriptPaBorar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptPaBorar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = Vector3.MoveTowards(this.transform.position, this.transform.position + Vector3.left, 5 * Time.deltaTime);
+    }
+}
